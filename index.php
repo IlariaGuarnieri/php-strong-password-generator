82f43e2aed894ff3e1cd76fc00e4dd9652bc889f
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-04-16 14:47:34 +0200

    [add] first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Strong Password Generator</title>
+</head>
+<body>
+
+  <div class="container my-5">
+    <h1>PHP Strong Password Generator</h1>
+  </div>
+
+  <div class="container my-5">
+    <div class="row">
+      <div class="col-6">
+        <form action="index.php">
+          
+        </form>
+      </div>
+    </div>
+  </div>
+  
+</body>
+</html>
